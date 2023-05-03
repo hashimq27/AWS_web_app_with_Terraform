b62823c80fe732e3090fe002f72175eacc8ec15b
hashimq27 <79986691+hashimq27@users.noreply.github.com>
2023-05-03 19:37:09 -0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ provider "aws" {
   access_key=var.AWS_ACCESS_KEY
   secret_key=var.AWS_SECRET_KEY
 }
-resources "aws_instance" "web" {
+resource "aws_instance" "web" {
   ami             = "ami-0da62eb5869c785b9"
   instance_type   = "t3.micro"
   
